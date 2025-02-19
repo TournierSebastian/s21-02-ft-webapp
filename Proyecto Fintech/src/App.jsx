28f7c86c2f825282184eecb9c656c72cd726f0c5
Morfeo <gastongomez2014@hotmail.com>
2025-02-19 00:40:16 -0300

    Inicio del proyecto en Vite y Tailwind

---
+++ Proyecto Fintech/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl text-blue-700'>Hello World!!!</h1>
+    </>
+  )
+}
+
+export default App
