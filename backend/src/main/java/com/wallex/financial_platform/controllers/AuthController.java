52d880a63d3002bb00a87fa0842af4f1cd6f6802
Delmer Teodomiro Rodriguez Gutierrez <jindrg@gmail.com>
2025-02-22 16:58:35 -0300

    Se corrige la inyeccion de dependencia  de AuthService

--- backend/src/main/java/com/wallex/financial_platform/controllers/AuthController.java
+++ backend/src/main/java/com/wallex/financial_platform/controllers/AuthController.java
@@ -3,11 +3,9 @@ package com.wallex.financial_platform.controllers;
 import com.wallex.financial_platform.dtos.requests.LoginRequestDTO;
 import com.wallex.financial_platform.dtos.requests.RegisterUserRequestDTO;
 import com.wallex.financial_platform.dtos.responses.UserResponseDTO;
-import com.wallex.financial_platform.entities.User;
 import com.wallex.financial_platform.services.impl.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private AuthService authService;
+    private final AuthService authService;
 
     @PostMapping("/register")
     public ResponseEntity<UserResponseDTO> register(@RequestBody RegisterUserRequestDTO registerUserRequestDTO) {
