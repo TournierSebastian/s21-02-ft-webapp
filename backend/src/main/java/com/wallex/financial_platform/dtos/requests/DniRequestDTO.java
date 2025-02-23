3aee75e09e5f211e744a36686feba42e6127bff8
Delmer Teodomiro Rodriguez Gutierrez <jindrg@gmail.com>
2025-02-22 22:34:19 -0300

    Se crean Controller y service de Card

--- backend/src/main/java/com/wallex/financial_platform/dtos/requests/DniRequestDTO.java
+++ backend/src/main/java/com/wallex/financial_platform/dtos/requests/DniRequestDTO.java
@@ -1,10 +1,8 @@
 package com.wallex.financial_platform.dtos.requests;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
-@Data
-public class DniRequestDTO {
-    @NotEmpty
-    private String dni;
-}
+
+public record DniRequestDTO(@NotBlank String dni) { }
