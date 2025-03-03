8f838e208c4b1a6b75c86cf9df59dca5957ffc5b
Delmer Teodomiro Rodriguez Gutierrez <jindrg@gmail.com>
2025-03-02 23:21:45 -0300

    Se mejora los estados de envio de la notificacion

--- backend/src/main/java/com/wallex/financial_platform/entities/listeners/NotificationListener.java
+++ backend/src/main/java/com/wallex/financial_platform/entities/listeners/NotificationListener.java
@@ -1,12 +1,17 @@
 package com.wallex.financial_platform.entities.listeners;
 
+import com.wallex.financial_platform.entities.Notification;
+import com.wallex.financial_platform.entities.enums.NotificationStatus;
 import com.wallex.financial_platform.events.NotificationEvent;
+import com.wallex.financial_platform.repositories.NotificationRepository;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -14,22 +19,30 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class NotificationListener {
     private static final Logger logger = LoggerFactory.getLogger(NotificationListener.class);
-
-    private final JavaMailSender emailSender;
+    private final JavaMailSender mailSender;
+    private final NotificationRepository notificationRepository;
 
     @Async
     @EventListener
     public void handleNotificationEvent(NotificationEvent event) {
+        Notification notification = event.getNotification();
         try {
-            SimpleMailMessage email = new SimpleMailMessage();
-            email.setTo(event.getUser().getEmail());
-            email.setSubject(event.getSubject());
-            email.setText(event.getMessage());
-            emailSender.send(email);
-
-            logger.info("Correo enviado a: {}", event.getUser().getEmail());
-        } catch (Exception e) {
-            logger.error("Error enviando correo a: {} - Error: {}", event.getUser().getEmail(), e.getMessage(), e);
+            sendEmail(notification.getUser().getEmail(), notification.getMessage());
+            notification.setStatus(NotificationStatus.SENT);
+            logger.info("Correo enviado a: {}", notification.getUser().getEmail());
+        } catch (MessagingException e) {
+            notification.setStatus(NotificationStatus.FAILED);
+            logger.error("Error enviando correo: {}", e.getMessage());
         }
+        notificationRepository.save(notification);
+    }
+
+    private void sendEmail(String to, String text) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(to);
+        helper.setSubject("Notificación");
+        helper.setText(text, true);
+        mailSender.send(message);
     }
 }
--- backend/src/main/java/com/wallex/financial_platform/services/impl/NotificationService.java
+++ backend/src/main/java/com/wallex/financial_platform/services/impl/NotificationService.java
@@ -6,6 +6,7 @@ import com.wallex.financial_platform.entities.enums.NotificationStatus;
 import com.wallex.financial_platform.entities.enums.NotificationType;
 import com.wallex.financial_platform.events.NotificationEvent;
 import com.wallex.financial_platform.repositories.NotificationRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +16,26 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
     private final ApplicationEventPublisher eventPublisher;
 
+    @Transactional
     public void notifyUser(User user, String subject, String message) {
-        eventPublisher.publishEvent(new NotificationEvent(this, user, subject, message));
+        Notification notification = new Notification();
+        notification.setUser(user);
+        notification.setType(NotificationType.EMAIL);
+        notification.setMessage(message);
+        notification.setStatus(NotificationStatus.PENDING);
+        notification.setSendingDate(LocalDateTime.now());
+
+        notificationRepository.save(notification);
+
+        eventPublisher.publishEvent(new NotificationEvent(this, notification));
     }
 }
