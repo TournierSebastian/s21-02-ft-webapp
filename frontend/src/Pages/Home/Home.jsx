d192201e029305af2ab01bd0f8c04b157d4250b2
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2025-03-10 13:12:39 -0300

    probando movimientos

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -8,8 +8,8 @@ import UseAccounts from '../../Hooks/Accounts/UseAccounts'
 import ActivityItem from '../../components/Activity/ActivityItem'
 import UseMovements from '../../Hooks/Movements/UseMovements'
 const Home = () => {
-  const {FetchAccountsbyid} = UseAccounts();
-  const {FetchMovementsbyuser} = UseMovements();
+  const { FetchAccountsbyid } = UseAccounts();
+  const { FetchMovementsbyuser } = UseMovements();
   const [Accounts, SetAccounts] = useState([]);
   const [Movements, SetMovements] = useState([])
   const [activities] = useState([
@@ -57,85 +57,82 @@ const Home = () => {
 
 
   const fetchData = async () => {
-          try {
-              const dataAccounts = await FetchAccountsbyid();
-              const dataMovements = await FetchMovementsbyuser();
-              SetMovements(dataMovements)
-              SetAccounts(dataAccounts || [])
-              console.log(dataMovements)
-          } catch (error) {
-              SetCurrencies([]);
-          }
-      };
-      useEffect(() => {
-  
-          fetchData();
-      }, [])
-  
+    try {
+      const dataAccounts = await FetchAccountsbyid();
+      const dataMovements = await FetchMovementsbyuser();
+      SetMovements(dataMovements)
+      SetAccounts(dataAccounts || [])
+      console.log(dataAccounts)
+    } catch (error) {
+      SetCurrencies([]);
+    }
+  };
+  useEffect(() => {
+
+    fetchData();
+  }, [])
+
   return (
     <div className='Contenido'>
-      <nav><Navbar/></nav>
+      <nav><Navbar /></nav>
       <main>
-      <div className='flex flex-col justify-center items-center mt-10'>
+        <div className='flex flex-col justify-center items-center mt-10'>
 
-      <div className=" w-full max-w-md bg-BlackBlue rounded-xl shadow-lg p-6 space-y-6 text-white">
-      {/* Gaston: Cabecera de la tarjeta*/}
-      <div className="flex justify-between items-center">
-        <h2 className="text-xl font-semibold">Dinero Disponible</h2>
-        <a className="px-4 py-1 bg-LightGolden text-gray-900 rounded-lg cursor-pointer hover:bg-DarkGolden transition-colors" href='/CVU'>
-          Tu CVU
-        </a>
-      </div>
+          <div className=" w-full max-w-md bg-BlackBlue rounded-xl shadow-lg p-6 space-y-6 text-white">
+            {/* Gaston: Cabecera de la tarjeta*/}
+            <div className="flex justify-between items-center">
+              <h2 className="text-xl font-semibold">Dinero Disponible</h2>
+              <a className="px-4 py-1 bg-LightGolden text-gray-900 rounded-lg cursor-pointer hover:bg-DarkGolden transition-colors" href='/CVU'>
+                Tu CVU
+              </a>
+            </div>
 
-      {/*Gaston: Dinero disponible*/}
-      <div className="text-3xl font-bold text-left">
-        ${Accounts.balance}
-      </div>
+            {/*Gaston: Dinero disponible*/}
+            <div className="text-3xl font-bold text-left">
+              ${Accounts.balance}
+            </div>
 
-      {/* Gaston: Botones de accion*/}
-      <div className="grid grid-cols-2 gap-4">
-        <button className="px-4 py-1 bg-LightGolden  cursor-pointer text-blue-900 rounded-lg hover:bg-DarkGolden transition-colors font-medium">
-          Ingresar Dinero
-        </button>
-        <button className="px-4 py-1 bg-LightGolden cursor-pointer text-blue-900 rounded-lg hover:bg-DarkGolden transition-colors font-medium">
-          Transferir Dinero
-        </button>
-      </div>
-        <div className='flex justify-between'>        
-      {/*Gaston: Marca de la tarjeta*/}
-      {/*Gaston: Falta agregar los iconos */}
-      <a className="w-3/4 border-LightGolden px-4 py-1 border-6 rounded-xl " href='/tutarjeta'>
-        <div className='flex flex-row gap-3 '>
-          <img src ={mastercard} className='w-10 h-10'></img>
-          <img src = {logo} className='w-10 h-10'></img>
-        </div>
-     
-        <div className='flex justify-between'>
-          <span className="text-1xl text-blue-200 italic ">Tarjeta Wallex</span>
-          <ChevronRightIcon size={20} color="#F5E7BE" strokeWidth={2} />
-        </div>
-      </a>
+            {/* Gaston: Botones de accion*/}
+            <div className="grid grid-cols-2 gap-4">
+              <button className="px-4 py-1 bg-LightGolden  cursor-pointer text-blue-900 rounded-lg hover:bg-DarkGolden transition-colors font-medium">
+                Ingresar Dinero
+              </button>
+              <button className="px-4 py-1 bg-LightGolden cursor-pointer text-blue-900 rounded-lg hover:bg-DarkGolden transition-colors font-medium">
+                Transferir Dinero
+              </button>
+            </div>
+            <div className='flex justify-between'>
+              {/*Gaston: Marca de la tarjeta*/}
+              {/*Gaston: Falta agregar los iconos */}
+              <a className="w-3/4 border-LightGolden px-4 py-1 border-6 rounded-xl " href='/tutarjeta'>
+                <div className='flex flex-row gap-3 '>
+                  <img src={mastercard} className='w-10 h-10'></img>
+                  <img src={logo} className='w-10 h-10'></img>
+                </div>
 
-        <img src={logo} className='w-15 h-15'></img>
-      </div>
-    </div>
-    
-    <div className="w-full max-w-md bg-BlackBlue rounded-3xl shadow-lg p-6 space-y-6 text-black mt-10">
-          <h2 className="text-xl font-semibold text-white">Tu última actividad</h2>
+                <div className='flex justify-between'>
+                  <span className="text-1xl text-blue-200 italic ">Tarjeta Wallex</span>
+                  <ChevronRightIcon size={20} color="#F5E7BE" strokeWidth={2} />
+                </div>
+              </a>
 
-          <div className="space-y-2 ">
-            {Movements.map((movements, index) => (
-              <ActivityItem key={index} {...movements} />
-            ))}
+              <img src={logo} className='w-15 h-15'></img>
+            </div>
           </div>
 
-          <button className="w-full text-xl py-3 text-gray-300 text-left px-2 rounded-lg cursor-pointer hover:text-white transition-colors font-medium">
-            Ver toda la actividad
-          </button>
+          <div className="w-full max-w-md bg-BlackBlue rounded-3xl shadow-lg p-6 space-y-6 text-black mt-10">
+            <h2 className="text-xl font-semibold text-white">Tu última actividad</h2>
+
+            <div className="space-y-2 ">
+              {Movements.map((movement, index) => (
+                <ActivityItem key={index} data={movement} />
+              ))}
+
+            </div>
+          </div>
         </div>
-  </div>
       </main>
-      
+
     </div>
   )
 }
--- frontend/src/components/Activity/ActivityItem.jsx
+++ frontend/src/components/Activity/ActivityItem.jsx
@@ -1,25 +1,66 @@
 
-function ActivityItem({ date, username, reason, amount, time, icon }) {
+function ActivityItem({data}) {
+  console.log("Activity Data:", data);
+
+
+  const formattedDate = data.createdAt.split("T")[0]; // Extrae solo la fecha
+  const [year, month, day] = formattedDate.split("-"); // Divide en año, mes y día
+
+  const monthNames = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+  ];
+  const monthName = monthNames[parseInt(month, 10) - 1]; // Convierte el número del mes a texto
+  const formattedDateWithMonthName = `${day} de ${monthName} de ${year}`;
+
+
+  const formattedTime = data.createdAt.split("T")[1].split(".")[0]; // Extrae la hora completa
+  const [hour, minutes] = formattedTime.split(":"); // Divide en horas y minutos
+  const formattedTimeWithoutSeconds = `${hour}:${minutes}`;
+//   {
+//     "movementId": 1,
+//     "transactionId": 1,
+//     "sourceAccount": {
+//         "cbu": "CBU000000035110000000000011",
+//         "alias": "otter.wood.chair",
+//         "currency": "ARS",
+//         "owner": "María Gómez",
+//         "dni": "87654321"
+//     },
+//     "destinationAccount": {
+//         "cbu": "CBU000000035160000000000024",
+//         "alias": "gnat.rubber.bed",
+//         "currency": "ARS",
+//         "owner": "Carlos López",
+//         "dni": "56789123"
+//     },
+//     "createdAt": "2025-03-10T12:31:45.830149",
+//     "description": "Movimiento generado para la transacción 1",
+//     "type": "TRANSFER",
+//     "status": "COMPLETED",
+//     "currency": "ARS",
+//     "amount": 1000
+// }
   return (
 
     <div className="border-b bg-white border-blue-800 rounded-3xl py-4 px-3">
-      <div className="text-sm text-gray-700 mb-2">{date}</div>
+       <div className="text-sm text-gray-700 mb-2">{formattedDateWithMonthName}</div>
       <div className="flex items-start justify-between">
         <div className="flex items-start gap-3">
           <div className="w-8 h-8  rounded-full flex items-center justify-center">
 
-            <span className="text-xl">{icon}</span>
+            <span className="text-xl"></span>
           </div>
           <div>
-            <p className="font-medium">{username}</p>
-            <p className="text-sm text-Gray">{reason}</p>
+            <p className="font-medium">{data.destinationAccount.owner}</p>
+            <p className="text-sm text-Gray">{data.type}</p>
           </div>
         </div>
         <div className="text-right">
-          <p className="font-medium">${amount}</p>
-          <p className="text-sm text-Gray">{time}</p>
+          <p className="font-medium">${data.amount}</p>
+          <p className="text-sm text-Gray">{formattedTimeWithoutSeconds}</p>
         </div>
-      </div>
+      </div> 
     </div>
   )
 }
